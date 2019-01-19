fac3a14d34ea78b3f0608369475c05b24b7ac1b7
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-01-19 21:00:53 +0000

    completed challenges

---
+++ challengethree.js
@@ -0,0 +1,24 @@
+// Challenge 3
+
+// on eir.ie
+
+var script = document.createElement('script');
+script.src = "//code.jquery.com/jquery-latest.min.js";
+document.head.appendChild(script);
+
+$("p").css("font-family"); //gt-walsheim-regular
+
+$("h2").css("font-family") //eir-medium
+
+$("p").text("pwodyk");
+
+
+// on Stack overflow
+
+$("body").css("background-color", "#333");
+$("div").css("background-color", "#333");
+$("header.top-bar").css("background-color", "#333");
+
+
+$("p").css("font-family");
+$("p").css("font-family", "Times, serif"); 
